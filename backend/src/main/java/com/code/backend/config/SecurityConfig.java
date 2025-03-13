604b9b8333bd800df06d5ce5f61fed67024a9860
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-13 15:24:20 +0900

    feat : 스웨거 403 에러 발견 후 csrf 비활성화

--- backend/src/main/java/com/code/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/code/backend/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.code.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/users/signUp", "/api/users/login").permitAll()
                         .anyRequest().authenticated()
