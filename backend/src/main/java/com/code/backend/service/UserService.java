b1621a67df64d535d20bdf2eca9c9de62128b0fa
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-13 15:18:43 +0900

    feat : 비밀번호 암호화

--- backend/src/main/java/com/code/backend/service/UserService.java
+++ backend/src/main/java/com/code/backend/service/UserService.java
@@ -3,25 +3,27 @@ package com.code.backend.service;
 import com.code.backend.entity.User;
 import com.code.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(String username, String password, String email) {
         User user = new User();
         user.setUsername(username);
-        user.setPassword(password);
+        user.setPassword(passwordEncoder.encode(password));
         user.setEmail(email);
         return userRepository.save(user);
     }
