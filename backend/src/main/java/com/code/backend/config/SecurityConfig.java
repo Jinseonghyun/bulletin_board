1ecbb74123ee5863000c964f0aeb58d8de16ce04
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-13 15:46:48 +0900

    feat : jwt 를 사용한 로그인 개발

--- backend/src/main/java/com/code/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/code/backend/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.code.backend.config;
 
+import com.code.backend.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailService userDetailService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,5 +35,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
     }
 }
--- backend/src/main/java/com/code/backend/controller/UserController.java
+++ backend/src/main/java/com/code/backend/controller/UserController.java
@@ -2,10 +2,17 @@ package com.code.backend.controller;
 
 import com.code.backend.dto.SignUpUser;
 import com.code.backend.entity.User;
+import com.code.backend.jwt.JwtUtil;
+import com.code.backend.service.CustomUserDetailService;
 import com.code.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +22,16 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailService userDetailService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager, JwtUtil jwtUtil, CustomUserDetailService userDetailService) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userDetailService = userDetailService;
     }
 
     @PostMapping("/")
@@ -37,5 +50,12 @@ public class UserController {
             @Parameter(description = "ID of the user to be deleted", required = true) @PathVariable Long userId) {
         userService.deleteUser(userId);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) throws AuthenticationException {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        UserDetails userDetails = userDetailService.loadUserByUsername(username);
+        return jwtUtil.generateToken(userDetails.getUsername());
     }
 }
