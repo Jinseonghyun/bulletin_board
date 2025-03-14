8384fd556b5e1e627cba080c1db6b7c759c6cb19
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-14 22:36:06 +0900

    feat : Security Context 사용하기 위해 User에서 UserDetails 받아줌

---
+++ backend/src/main/java/com/code/backend/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.code.backend.repository;
+
+import com.code.backend.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
