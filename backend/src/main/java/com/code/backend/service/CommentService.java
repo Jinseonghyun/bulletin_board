9037af426579a7e410b937d34c07917f01627783
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-15 18:33:35 +0900

    feat : 댓글 조회 기능 개발
    - 비동기형 호출 방식을 사용하여 동시에 쿼리 실행 가능 하도록

--- backend/src/main/java/com/code/backend/service/CommentService.java
+++ backend/src/main/java/com/code/backend/service/CommentService.java
@@ -13,6 +13,7 @@ import com.code.backend.repository.BoardRepository;
 import com.code.backend.repository.CommentRepository;
 import com.code.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,7 +24,10 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class CommentService {
@@ -76,6 +80,7 @@ public class CommentService {
         commentRepository.save(comment);
         return comment;
     }
+
     private boolean isCanWriteComment() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
@@ -104,5 +109,41 @@ public class CommentService {
         Duration duration = Duration.between(localDateTime, dateAsLocalDateTime);
 
         return Math.abs(duration.toMinutes()) > 1;
+    }
+
+    @Async
+    protected CompletableFuture<Article> getArticle(Long boardId, Long articleId) {
+        Optional<Board> board = boardRepository.findById(boardId);
+        if (board.isEmpty()) {
+            throw new ResourceNotFoundException("board not found");
+        }
+        Optional<Article> article = articleRepository.findById(articleId);
+        if (article.isEmpty() || article.get().getIsDeleted()) {
+            throw new ResourceNotFoundException("article not found");
+        }
+        return CompletableFuture.completedFuture(article.get());
+    }
+
+    @Async
+    protected CompletableFuture<List<Comment>> getComments(Long articleId) {
+        return CompletableFuture.completedFuture(commentRepository.findByArticleId(articleId));
+    }
+
+    public CompletableFuture<Article> getArticleWithComment(Long boardId, Long articleId) {
+        CompletableFuture<Article> articleFuture = this.getArticle(boardId, articleId);
+        CompletableFuture<List<Comment>> commentsFuture = this.getComments(articleId);
+
+        return CompletableFuture.allOf(articleFuture, commentsFuture)
+                .thenApply(voidResult -> {
+                    try {
+                        Article article = articleFuture.get();
+                        List<Comment> comments = commentsFuture.get();
+                        article.setComments(comments);
+                        return article;
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                });
     }
 }
