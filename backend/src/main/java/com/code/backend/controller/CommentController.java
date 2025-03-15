e54371f7843a837e416534485fac83d04654e67a
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-15 16:54:40 +0900

    feat : 댓글 작성 기능 개발
    - 삭제된 게시글인지 체크
    - 동시성 처리 위한 트랜잭션 처리

---
+++ backend/src/main/java/com/code/backend/controller/CommentController.java
@@ -0,0 +1,29 @@
+package com.code.backend.controller;
+
+import com.code.backend.dto.WriteCommentDto;
+import com.code.backend.entity.Comment;
+import com.code.backend.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/boards")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/{boardId}/articles/{articleId}")
+    public ResponseEntity<Comment> writeComment(@PathVariable Long boardId,
+                                                @PathVariable Long articleId,
+                                                @RequestBody WriteCommentDto writeCommentDto) {
+        return ResponseEntity.ok(commentService.writeComment(boardId, articleId, writeCommentDto));
+    }
+
+
+}
