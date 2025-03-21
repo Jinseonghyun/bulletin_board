7f055a581a1480069510f019ceb409dd82e28b16
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-21 16:55:24 +0900

    feat: 유저의 기기를 등록하고 조회하는 로직 개발

---
+++ backend/src/main/java/com/code/backend/dto/WriteDeviceDto.java
@@ -0,0 +1,9 @@
+package com.code.backend.dto;
+
+import lombok.Getter;
+
+@Getter
+public class WriteDeviceDto {
+    private String deviceName;
+    private String token;
+}
--- backend/src/main/java/com/code/backend/service/UserService.java
+++ backend/src/main/java/com/code/backend/service/UserService.java
@@ -1,13 +1,20 @@
 package com.code.backend.service;
 
 import com.code.backend.dto.SignUpUser;
+import com.code.backend.dto.WriteDeviceDto;
+import com.code.backend.entity.Device;
 import com.code.backend.entity.User;
 import com.code.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -35,5 +42,31 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public List<Device> getDevices() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
+        if (user.isPresent()) {
+            return user.get().getDeviceList();
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    public Device addDevice(WriteDeviceDto writeDeviceDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
+        if (user.isPresent()) {
+            Device device = new Device();
+            device.setDeviceName(writeDeviceDto.getDeviceName());
+            device.setToken(writeDeviceDto.getToken());
+            user.get().getDeviceList().add(device);
+            userRepository.save(user.get());
+            return device;
+        }
+        return null;
     }
 }
