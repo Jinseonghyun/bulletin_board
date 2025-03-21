48ba1f09983d892300b8d94abd0b68e91a62294b
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-21 23:45:55 +0900

    feat : MongoDB Document 사용할 때 collection 설정 수정 및 개인 알림  스키마 추가

--- backend/src/main/java/com/code/backend/entity/AdClickHistory.java
+++ backend/src/main/java/com/code/backend/entity/AdClickHistory.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Document(collation = "adClickHistory")
+@Document(collection = "adClickHistory")
 @Getter
 @Setter
 public class AdClickHistory {
